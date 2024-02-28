6686c2f0d20ac9d99a7f5f7279f43cbde24df444
jjaeho0415 <jjh0773@naver.com>
2024-02-28 12:30:47 +0900

    redux-tookit 사용해보기

---
+++ src/features/counter/Test.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { useAppDispatch } from "../../app/hooks";
+import { fetchUsersAsync } from "./counterSlice";
+
+const Test = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    const promise = dispatch(fetchUsersAsync());
+    // const promise = dispatch(incrementAsync(10));
+    return () => {
+      promise.abort();
+    };
+  });
+  return <div>Test</div>;
+};
+
+export default Test;
